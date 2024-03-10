22be505297a8480a518a443b54b7030baa23adc5
Roman Mesler <pay2mesler@gmail.com>
2024-03-10 15:26:05 +0500

    fix all

--- src/main/java/com/course/springhomeworks/controller/ProductController.java
+++ src/main/java/com/course/springhomeworks/controller/ProductController.java
@@ -1,41 +1,46 @@
 package com.course.springhomeworks.controller;
 
 import com.course.springhomeworks.exception.ProductNotFoundException;
-import com.course.springhomeworks.model.DTO.InfoDTO;
 import com.course.springhomeworks.model.DTO.ProductDTO;
 import com.course.springhomeworks.model.Info;
 import com.course.springhomeworks.model.Product;
 import com.course.springhomeworks.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class ProductController {
-    private static final List<Product> database = Collections.synchronizedList(new ArrayList<>());
+    private static final Map<Integer, Product> database = new ConcurrentHashMap<>();
+
     static {
-        database.add(new Product(20.0, new Info(1, LocalDate.parse("2022-01-01"))));
-        database.add(new Product(12.0, new Info(2,  LocalDate.parse("2022-01-02"))));
-        database.add(new Product(1.0, new Info(3,  LocalDate.parse("2022-01-03"))));
+        Product product1 = new Product(20.0, new Info(1, LocalDate.parse("2022-01-01")));
+        database.put(database.size() + 1, product1);
+        Product product2 = new Product(12.0, new Info(2, LocalDate.parse("2022-01-02")));
+        database.put(database.size() + 1, product2);
+        Product product3 = new Product(1.0, new Info(3, LocalDate.parse("2022-01-03")));
+        database.put(database.size() + 1, product3);
     }
+
     private final ProductService productService;
+
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
     @PostMapping("/product")
-    public Product addProduct(@RequestBody ProductDTO productDTO){
+    public Product addProduct(@RequestBody ProductDTO productDTO) {
         return productService.addToDataBase(productDTO, database);
     }
 
     @GetMapping("/product/{id}")
-    public Product getProductById(@PathVariable double id){
-        return database.stream().
-                filter(product -> product.getInfo().getId() == id).
-                findFirst().orElseThrow(() -> new ProductNotFoundException(id));
+    public Product getProductById(@PathVariable int id) throws ProductNotFoundException {
+        if (id < 0 || id > database.size()) {
+            throw new ProductNotFoundException(id);
+        } else {
+            return database.get(id);
+        }
     }
 }
--- src/main/java/com/course/springhomeworks/service/ProductService.java
+++ src/main/java/com/course/springhomeworks/service/ProductService.java
@@ -6,19 +6,19 @@ import com.course.springhomeworks.model.Info;
 import com.course.springhomeworks.model.Product;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProductService {
-    public Product addToDataBase(ProductDTO productDTO, List<Product> database){
+    public Product addToDataBase(ProductDTO productDTO, Map<Integer,Product> database){
         Product product = new Product();
         product.setPrice(productDTO.getPrice());
         Info info = new Info();
         InfoDTO infoDTO = productDTO.getInfoDTO();
         info.setDate(infoDTO.getDate());
-        info.setId(database.get(database.size() - 1).getInfo().getId() + 1); // берем последнюю запись смотрим на её id и прибавляем 1
+        info.setId(database.get(database.size()).getInfo().getId() + 1); // берем последнюю запись смотрим на её id и прибавляем 1
         product.setInfo(info);
-        database.add(product);
+        database.put(database.size() + 1, product);
         return product;
     }
 }
