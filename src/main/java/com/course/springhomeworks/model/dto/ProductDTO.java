6d32d800879667d1a1367a7565bb4dd510acc5d7
Alexeev Nikita <nakinaki243@gmail.com>
2024-03-10 15:42:05 +0500

    resolve problems

--- src/main/java/com/course/springhomeworks/model/dto/ProductDTO.java
+++ src/main/java/com/course/springhomeworks/model/dto/ProductDTO.java
@@ -2,12 +2,10 @@ package com.course.springhomeworks.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
 public class ProductDTO {
     @JsonProperty("price")
     private double price;
--- src/main/java/com/course/springhomeworks/model/dto/ProductInfoDTO.java
+++ src/main/java/com/course/springhomeworks/model/dto/ProductInfoDTO.java
@@ -2,16 +2,12 @@ package com.course.springhomeworks.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.util.Date;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
 public class ProductInfoDTO {
     @JsonProperty("date")
     private LocalDate date;
