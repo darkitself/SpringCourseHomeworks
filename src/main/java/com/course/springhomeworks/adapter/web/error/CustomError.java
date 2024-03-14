c6bbf91d4770adaec091c3446f9ba480caab17c7
Waranim <129619485+Waranim@users.noreply.github.com>
2024-03-14 18:26:49 +0500

    refactoring

--- src/main/java/com/course/springhomeworks/adapter/web/error/CustomError.java
+++
@@ -1,8 +0,0 @@
-package com.course.springhomeworks.adapter.web.error;
-
-import lombok.Getter;
-
-@Getter
-public class CustomError extends RuntimeException{
-    private final String errorMessage = "¯\\_( ͡° ͜ʖ ͡°)_/¯";
-}
