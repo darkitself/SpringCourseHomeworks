7bcfa789016f358f5abfdf7107830cf63b951af9
Roman Mesler <pay2mesler@gmail.com>
2024-03-16 16:41:31 +0500

    fix

--- src/main/java/com/course/springhomeworks/adapter/web/validation/IsJavaDevValidator.java
+++ src/main/java/com/course/springhomeworks/adapter/web/validation/IsJavaDevValidator.java
@@ -3,11 +3,10 @@ package com.course.springhomeworks.adapter.web.validation;
 import com.course.springhomeworks.adapter.web.dto.PersonDTO;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import jakarta.validation.constraints.NotNull;
 
 public class IsJavaDevValidator implements ConstraintValidator<IsJavaDevConstraint, PersonDTO> {
     @Override
-    public boolean isValid(@NotNull(message = "не может быть пустым") PersonDTO personDTO,
+    public boolean isValid(PersonDTO personDTO,
                            ConstraintValidatorContext constraintValidatorContext) {
         return personDTO.isJavaBackEndDev();
     }
--- src/main/java/com/course/springhomeworks/adapter/web/validation/NameConstraint.java
+++ src/main/java/com/course/springhomeworks/adapter/web/validation/NameConstraint.java
@@ -2,6 +2,8 @@ package com.course.springhomeworks.adapter.web.validation;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -10,9 +12,11 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
+@NotBlank(message = "имя не должно быть пустым")
+@Size(max = 50, message = "Имя/Фамилия не должны превышать 50 символов")
 @Constraint(validatedBy = NameValidator.class)
 public @interface NameConstraint {
-    String message() default "Имя/Фамилия должны быть не пустыми, не превышать 20 знаков, не должны содержать цифр";
+    String message() default "Имя/Фамилия не должны превышать 20 знаков, не должны содержать цифр";
 
     Class<?>[] groups() default {};
 
