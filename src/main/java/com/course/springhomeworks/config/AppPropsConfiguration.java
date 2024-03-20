e19d92ef6ad60813473719dc97d5570e0325a758
Roman Mesler <pay2mesler@gmail.com>
2024-03-20 20:26:27 +0500

    fix

--- src/main/java/com/course/springhomeworks/config/AppPropsConfiguration.java
+++ src/main/java/com/course/springhomeworks/config/AppPropsConfiguration.java
@@ -1,10 +1,11 @@
 package com.course.springhomeworks.config;
 
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import com.course.springhomeworks.config.property.AppProps;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationPropertiesScan
+@EnableConfigurationProperties(AppProps.class)
 public class AppPropsConfiguration {
 
 }
